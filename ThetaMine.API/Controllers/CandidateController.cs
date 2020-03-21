73e7b442f7e14837a4e341327f1fbba409f1500d
Abhinav Pradeep <Abhinav.Pradeep@hotmail.com>
2020-03-21 11:43:07 +1000

    API

---
+++ ThetaMine.API/Controllers/CandidateController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ThetaMine.Models.Core;
+using ThetaMine.Repos;
+
+namespace ThetaMine.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CandidateController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult ListCandidates()
+        {
+            CandidateRepos candidateRepos = new CandidateRepos("mongodb+srv://appuser:apppassword@clusterone-5rfg7.mongodb.net/test?retryWrites=true&w=majority");
+            var candidates = candidateRepos.GetAllCandidates();
+            return new JsonResult(candidates);
+        }
+        [HttpGet("{candidateID}")]
+        public Candidate GetCandidate(string candidateID)
+        {
+            CandidateRepos candidateRepos = new CandidateRepos("mongodb+srv://appuser:apppassword@clusterone-5rfg7.mongodb.net/test?retryWrites=true&w=majority");
+            var candidate = candidateRepos.GetCandidate(candidateID);
+            return candidate;
+        }
+        [HttpDelete("{candidateID}")]
+        public long DeleteCandidate(string candidateID)
+        {
+            CandidateRepos candidateRepos = new CandidateRepos("mongodb+srv://appuser:apppassword@clusterone-5rfg7.mongodb.net/test?retryWrites=true&w=majority");
+            var DeletedCount = candidateRepos.DeleteCandidate(candidateID);
+            return DeletedCount;
+        }
+        [HttpPost]
+        public ActionResult TestAddCandidate()
+        {
+            CandidateRepos candidateRepos = new CandidateRepos("mongodb+srv://appuser:apppassword@clusterone-5rfg7.mongodb.net/test?retryWrites=true&w=majority");
+            EmailID email = new EmailID("abhinav.pradeep", "gmail.com");
+            email.IsPrimary = true;
+            Candidate candidate = new Candidate();
+            EmailIDs emailIDs = new EmailIDs();
+            EmailID email2 = new EmailID("abhinav.pradeep", "hotmail.com");
+            email2.IsPrimary = false;
+            emailIDs.emailIDset.Add(email);
+            emailIDs.emailIDset.Add(email2);
+            candidate.emailIDs = emailIDs;
+            candidateRepos.AddCandidate(candidate);
+            return Ok();
+        }
+    }
+}
